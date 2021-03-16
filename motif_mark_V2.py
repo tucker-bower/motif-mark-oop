569656cd96e6e7c2c9cecb0e745fdbe600598b85
tucker.bower@gmail.com <tucker.bower@gmail.com>
2021-03-16 02:23:15 -0700

    added more classes

--- motif_mark_V2.py
+++ motif_mark_V2.py
@@ -38,19 +38,44 @@ IUPAC_CODES_DICT = {'A':'[Aa]', 'T':'[TtUu]', 'U':'[TtUu]', 'C':'[Cc]', 'G':'[Gg
 ### global variables for pycairo ###
 LEFT_MARGIN = 10
 
+def color_brew_inator():
+    '''Loads the color palette in from a user given file'''
+    colors_list = []
+    while True:
+        line =  COLORS_FILE.readline().rstrip()
+        if line == '':
+            break
+        color_rgb_values = re.findall('[0-9]{1,3}', line)
+        colors_list.append(color_rgb_values)
+    return colors_list
+    #grabbing all of the colors. 
+    #   The rgb values of each color are a list. 
+    #   All color lists are stored in an outer list (COLORS_LIST)'
+
+COLORS_LIST = color_brew_inator()
+
+def make_colors_ints_inator():
+    '''Changes the rgb values in the colors list from strings to ints'''
+    colors_ints_list = []
+    for color in COLORS_LIST:
+        color_ints = []
+        for rgb_string in color:
+            rgb_int = int(rgb_string)
+            color_ints.append(rgb_int)
+        colors_ints_list.append(color_ints)
+    return colors_ints_list
+
+COLORS_INTS_LIST = make_colors_ints_inator()
+
+print(COLORS_INTS_LIST)
 
 class Gene:
-    
+    '''contains header, gene length (total read from fasta, and gene #, method 
+    to draw itself'''
     def __init__(self, header, length, gene_count):
-        '''1 gene = 1 'read' from the .Fasta file, contains introns, exons, motifs'''
-
-        ### Data ###
-
         self.header = header
         self.length = length
         self.gene_count = gene_count
-
-        ### Methods ### 
 
     def draw(self):
         x = LEFT_MARGIN
@@ -67,6 +92,36 @@ class Gene:
 
         #context.show_text(gene)
 
+class Exon:
+    '''contains start_pos_pos, gene length (total read from fasta), length,
+     and gene # & method to draw itself'''
+    def __init__(self, start_pos, length, gene_count):
+        self.start_pos = start_pos
+        self.length = length
+        self.gene_count = gene_count
+
+    def draw(self):
+        x = LEFT_MARGIN + self.start_pos
+        y = self.gene_count * 30 + 30
+        context.set_line_width(20)
+        context.move_to(x ,y)
+        context.line_to(x+self.length, y)
+        context.stroke()
+
+class Motif:
+    '''contains start, length, gene_count)'''
+    def __init__(self, start_pos, length, gene_count):
+        self.start_pos = start_pos
+        self.length = length
+        self.gene_count = gene_count
+
+    def draw(self):
+        x = LEFT_MARGIN + self.start_pos
+        y = self.gene_count * 30 + 30
+        context.set_line_width(20)
+        context.move_to(x ,y)
+        context.line_to(x+self.length, y)
+        context.stroke()
 #
 ### PLACEHOLDER CODE ###
 #
@@ -86,6 +141,7 @@ context.set_font_size(12)
 def beautiful_picture_inator():
     gene_examp = Gene('foo', 800, 1)
     gene_examp.draw()
-
+    exon_examp = Exon(100,100,1)
+    exon_examp.draw()
 beautiful_picture_inator()
 
